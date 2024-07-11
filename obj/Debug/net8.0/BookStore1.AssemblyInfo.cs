7eede3d3701a15ac4d040874b23505d09a30a96d
sssels <selturkmen88@gmail.com>
2024-07-11 21:14:12 +0300

    changes

--- obj/Debug/net8.0/BookStore1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookStore1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7899923f6ebf921254b08e4b3824bad68f010034")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5527967225d52342958b02ae97627cbc6b1fa1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
